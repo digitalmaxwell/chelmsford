e07aefcb2ff54b95d00488d8f2bf28b7815168b0
Maxwell Chandler <personal@digitalmaxwell.com>
2023-07-16 16:14:57 -0400

    Media block updates for new variation, alt tags, collapsable header for mobile

--- components/header.js
+++ components/header.js
@@ -1,58 +1,79 @@
 class Header extends HTMLElement {
   constructor() {
-    super();
-    this._selectedNavItem = null;
+      super();
+      this._selectedNavItem = null;
+      this._isNavOpen = false;
+      this._toggleButton = null;
   }
 
   static get observedAttributes() {
-    return ['selected-nav-item'];
+      return ['selected-nav-item'];
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    if (name === 'selected-nav-item') {
-      this.selectedNavItem = newValue;
-    }
-  }
-
-  get selectedNavItem() {
-    return this._selectedNavItem;
-  }
-
-  set selectedNavItem(value) {
-    this._selectedNavItem = value;
-    this.updateSelectedNavItem();
+      if (name === 'selected-nav-item') {
+          this.selectedNavItem = newValue;
+      }
   }
 
   connectedCallback() {
-    this.innerHTML = `
-      <header>
-          <div class="header__nativeAmericans">
-              <div class="container">
+      this.render();
+      this.addEventListeners();
+      this.updateSelectedNavItem();
+  }
+
+  render() {
+      this.innerHTML = `
+          <header>
+              <div class="header__nativeAmericans">
                   <h3>Native Americans</h3>
               </div>
-          </div>
-          <nav class="header__nav">
-              <ul class="container">
-                  <li><a href="history.html" class="p--md">History in the Merrimack River Drainage</a></li>
-                  <li><a href="timeline.html" class="p--md">Cultural timeline</a></li>
-                  <li><a href="importance-of-sites.html" class="p--md">Importance of Archaeological sites</a></li>
-                  <li><a href="finding-and-studying-sites.html" class="p--md">Finding and Studying sites</a></li>
-                  <li><a href="learn-more.html" class="p--md">Learn more</a></li>
-              </ul>
-          </nav>
-      </header>
-    `;
-    this.updateSelectedNavItem();
+              <nav class="header__nav">
+                  <div class="toggle-button">
+                      <div class="bars">
+                        <div class="bar"></div>
+                        <div class="bar"></div>
+                        <div class="bar"></div>
+                      </div>
+                  </div>
+                  <ul class="container">
+                      <li><a class="p--md" href="history.html">History in the Merrimack River Drainage</a></li>
+                      <li><a class="p--md" href="timeline.html">Cultural timeline</a></li>
+                      <li><a class="p--md" href="importance-of-sites.html">Importance of Archaeological sites</a></li>
+                      <li><a class="p--md" href="finding-and-studying-sites.html">Finding and Studying sites</a></li>
+                      <li><a class="p--md" href="learn-more.html">Learn more</a></li>
+                  </ul>
+              </nav>
+          </header>
+      `;
+  }
+
+  addEventListeners() {
+      this._toggleButton = this.querySelector('.toggle-button');
+      this._toggleButton.addEventListener('click', this.toggleNav.bind(this));
+  }
+
+  toggleNav() {
+      this._isNavOpen = !this._isNavOpen;
+      this.querySelector('.header__nav ul').classList.toggle('nav-open', this._isNavOpen);
+      this._toggleButton.classList.toggle('active', this._isNavOpen);
   }
 
   updateSelectedNavItem() {
-    // Remove the 'selected' class from any existing element
-    const existingSelected = this.querySelector('li.selected');
-    if (existingSelected) existingSelected.classList.remove('selected');
-  
-    // Add the 'selected' class to the new selected item
-    const newItem = this.querySelector(`a[href="${this._selectedNavItem}"]`);
-    if (newItem) newItem.parentElement.classList.add('selected');
+      const existingSelected = this.querySelector('li.selected');
+      if (existingSelected) existingSelected.classList.remove('selected');
+
+      const newItem = this.querySelector(`a[href="${this._selectedNavItem}"]`);
+      if (newItem) newItem.parentElement.classList.add('selected');
+  }
+
+  get selectedNavItem() {
+      return this._selectedNavItem;
+  }
+
+  set selectedNavItem(value) {
+      this._selectedNavItem = value;
+      this.updateSelectedNavItem();
   }
 }
 
--- components/mediaBlock.js
+++ components/mediaBlock.js
@@ -5,7 +5,10 @@ class MediaBlock extends HTMLElement {
     }
 
     static get observedAttributes() {
-        return ['image', 'imagewidth', 'imagetext', 'header', 'body', 'body2', 'subtext', 'reverse'];
+        return [
+            'image', 'imagealt', 'imagetext', 'image2', 'image2alt', 'image2text', 'imagewidth', 
+            'header', 'body', 'body2', 'subtext', 'reverse'
+        ];
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
@@ -27,18 +30,28 @@ class MediaBlock extends HTMLElement {
 
     render() {
         this.innerHTML = `
-            <div class="${this.getProp('reverse') ? 'mediaBlock reverse' : 'mediaBlock'}">
+            <div class="${this.getProp('reverse') ? 'mediaBlock reverse' : 'mediaBlock'} ${this.getProp('image2') ? 'mediaBlock__images' : ""}">
                 <div class="container">
-                    <div class="mediaBlock__text">
-                        ${this.getProp('header') ? `<h2 class="heading2">${this.getProp('header')}</h2>` : ""}
-                        ${this.getProp('body') ? `<p class="p--lg">${this.getProp('body')}</p>` : ""}
-                        ${this.getProp('body2') ? `<p class="p--lg">${this.getProp('body2')}</p>` : ""}
-                        ${this.getProp('subtext') ? `<p class="p--sm">${this.getProp('subtext')}</p>` : ""}
-                    </div>
-                    <div class="mediaBlock__image" ${this.getProp('imagewidth') ? "style='width: " + this.getProp('imagewidth') + ";'" : ""}>
-                        ${this.getProp('image') ? `<img src="${this.getProp('image')}" alt="Territory Map" />` : ""}
-                        ${this.getProp('imagetext') ? `<p class="p--sm">${this.getProp('imagetext')}</p>` : ""}
-                    </div>
+                    ${!this.getProp('image2') ?
+                        ` <div class="mediaBlock__text">
+                            ${this.getProp('header') ? `<h2 class="heading2">${this.getProp('header')}</h2>` : ""}
+                            ${this.getProp('body') ? `<p class="p--lg">${this.getProp('body')}</p>` : ""}
+                            ${this.getProp('body2') ? `<p class="p--lg">${this.getProp('body2')}</p>` : ""}
+                            ${this.getProp('subtext') ? `<p class="p--sm">${this.getProp('subtext')}</p>` : ""}
+                        </div>`
+                    : ""}
+                    ${this.getProp('image') && this.getProp('imagealt')?
+                        `<div class="mediaBlock__image" ${this.getProp('imagewidth') ? "style='width: " + this.getProp('imagewidth') + ";'" : ""}>
+                                <img src="${this.getProp('image')}" alt="${this.getProp('imagealt')}" />
+                                ${this.getProp('imagetext') ? `<p class="p--sm">${this.getProp('imagetext')}</p>` : ""}
+                        </div>`
+                    : ""}
+                    ${this.getProp('image2') && this.getProp('image2alt') ?
+                        `<div class="mediaBlock__image" ${this.getProp('imagewidth') ? "style='width: " + this.getProp('imagewidth') + ";'" : ""}>
+                                <img src="${this.getProp('image2')}" alt="${this.getProp('image2alt')}" />
+                                ${this.getProp('image2text') ? `<p class="p--sm">${this.getProp('image2text')}</p>` : ""}
+                        </div>`
+                    : ""}
                 </div>
             </div>
         `;
